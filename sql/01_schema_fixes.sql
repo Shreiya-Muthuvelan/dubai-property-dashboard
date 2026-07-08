46dd87e5991ecbfe366f27a65d0a255c337c1b19
Shreiya Muthuvelan <shreiyavel@gmail.com>
2026-07-08 18:07:39 +0530

    Alter columns in transactions table for data type changes

--- sql/01_schema_fixes.sql
+++ sql/01_schema_fixes.sql
@@ -1,4 +1,3 @@
-
 ALTER TABLE transactions ALTER COLUMN has_parking SET DATA TYPE BOOLEAN USING (has_parking::BOOLEAN);
 ALTER TABLE transactions ALTER COLUMN instance_date SET DATA TYPE DATE USING (instance_date::DATE);
 ALTER TABLE transactions ALTER COLUMN actual_worth TYPE DOUBLE USING TRY_CAST(actual_worth AS DOUBLE);
